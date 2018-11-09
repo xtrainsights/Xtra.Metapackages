cdb0607388d336eef362156f865f283895dd8159
Michael Monsour <michael@lethek.net>
2018-11-09 10:46:06 +1000

    First commit: added an Xtra.Metapackages.Serilog NuGet metapackage

---
+++ src/Serilog.Enrichers.ApplicationName/ApplicationNameEnricher.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.PlatformAbstractions;
+
+using Serilog.Core;
+using Serilog.Events;
+
+
+namespace Serilog.Enrichers.ApplicationName
+{
+
+    internal class ApplicationNameEnricher : ILogEventEnricher
+    {
+
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            if (_applicationName == null) {
+                var env = PlatformServices.Default.Application;
+                _applicationName = propertyFactory.CreateProperty(nameof(env.ApplicationName), env.ApplicationName);
+            }
+
+            logEvent.AddPropertyIfAbsent(_applicationName);
+        }
+
+
+        private LogEventProperty _applicationName;
+
+    }
+
+}
---
+++ src/Serilog.Enrichers.ApplicationName/ExtendLoggerEnrichmentConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Serilog.Configuration;
+
+
+namespace Serilog.Enrichers.ApplicationName
+{
+
+    public static class ExtendLoggerEnrichmentConfiguration
+    {
+        public static LoggerConfiguration WithApplicationName(this LoggerEnrichmentConfiguration enrichmentConfig)
+            => enrichmentConfig == null
+                ? throw new ArgumentNullException(nameof(enrichmentConfig))
+                : enrichmentConfig.With<ApplicationNameEnricher>();
+    }
+
+}
